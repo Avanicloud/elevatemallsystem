8c8ce3018831d6f6a6d9d96c5bca7ffe10bfb9fb
Avani <59282280+Avanicloud@users.noreply.github.com>
2020-02-18 16:26:35 +0530

    Add files via upload

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Elevate_Mall_System.Startup))]
+namespace Elevate_Mall_System
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
